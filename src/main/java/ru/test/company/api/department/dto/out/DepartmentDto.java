d7048de98dc34f2ad21a94005bb5d1e9beb8c35f
Kasyanov KS <fierymail@list.ru>
2020-03-05 09:23:33 +0300

    Вернул DepartmentDto, который был случайно помечен игнорируемым в git

---
+++ src/main/java/ru/test/company/api/department/dto/out/DepartmentDto.java
@@ -0,0 +1,31 @@
+package ru.test.company.api.department.dto.out;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Полная ДТО для сущности "Отдел"
+ * */
+
+@Getter
+@Setter
+@ApiModel("Отдел")
+public class DepartmentDto {
+
+    @ApiModelProperty("Идентификатор отдела")
+    private UUID id;
+
+    @ApiModelProperty("Название отдела")
+    private String name;
+
+    @ApiModelProperty("Дата первого рабочего дня отдела")
+    private LocalDateTime firstWorkingDate;
+
+    @ApiModelProperty("Дата последнего рабочего дня отдела")
+    private LocalDateTime lastWorkingDate;
+
+}
