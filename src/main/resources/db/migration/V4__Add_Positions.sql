34a75dcfff372510e513216e292cb51202ca4c32
Kasyanov KS <fierymail@list.ru>
2020-03-07 19:43:19 +0300

    Поправил миграции. Добавил создание должностей

---
+++ src/main/resources/db/migration/V4__Add_Positions.sql
@@ -0,0 +1,10 @@
+insert into position (name, id)
+    values ('system_administrator', '1c3e2dc2-a789-4135-b0b3-b1176229ce7x');
+insert into position (name, id)
+    values ('java_programmer', '2c3e2dc2-a789-4135-b0b3-b1176229ce7x');
+insert into position (name, id)
+    values ('python_programmer', '3c3e2dc2-a789-4135-b0b3-b1176229ce7x');
+insert into position (name, id)
+    values ('manager', '4c3e2dc2-a789-4135-b0b3-b1176229ce7x');
+insert into position (name, id)
+    values ('director', '5c3e2dc2-a789-4135-b0b3-b1176229ce7x');
